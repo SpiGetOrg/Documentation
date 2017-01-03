02cccd3f825ddda04a51efa63192f8ddfef308c0
Haylee Schäfer <mail@inventivetalent.org>
2017-01-03 20:06:03 +0100

    Update push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+git config user.name "Travis"
+git config user.email "${GITHUB_EMAIL}"
+
 git add swagger.json
-git push
+git commit -m "[CI] Generate swagger.json"
+git push --force --quiet "https://${GITHUB_TOKEN}@${GITHUB_REPO}" master
