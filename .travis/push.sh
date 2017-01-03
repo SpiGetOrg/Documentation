2b0140c73d0b673823a9b11635bb6e36988fd5ed
Haylee Schäfer <mail@inventivetalent.org>
2017-01-03 20:22:43 +0100

    Update push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,6 +3,7 @@
 git config user.name "Travis"
 git config user.email "${GITHUB_EMAIL}"
 
+git init
 git add swagger.json
 git commit -m "[CI] Generate swagger.json" || echo "Commit failed (There probably were no changes)"
 git push --force "https://InventiveBot:${GITHUB_TOKEN}@${GITHUB_REPO}" master
