904c325f223d5ba55e1c2703fc0cf0ecea98591d
Haylee Schäfer <mail@inventivetalent.org>
2017-01-03 20:30:56 +0100

    Update swagger.sh

--- swagger.sh
+++ swagger.sh
@@ -9,5 +9,5 @@ wget -nc http://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.1.6/swag
 java -jar swagger-codegen-cli.jar generate -i swagger.yml -l swagger
 
 git add swagger.json
-git commit -m "[CI] Generate swagger.json" || echo "Commit failed (There probably were no changes)"
+git commit -m "Generate swagger.json [ci skip]" || echo "Commit failed (There probably were no changes)"
 git push --force "https://InventiveBot:${GITHUB_TOKEN}@${GITHUB_REPO}" master
