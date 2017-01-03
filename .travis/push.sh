e5f8687354b1d060c3bb47118a9d0c70ecf31fbc
Haylee Schäfer <mail@inventivetalent.org>
2017-01-03 19:46:05 +0100

    Update push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -2,6 +2,6 @@
 
 git config --global user.email "${GITHUB_USER}"
 
-git remote set-url origin https://${GITHUB_USER}:${GITHUB_TOKEN}/SpiGetOrg/Documentation.git
+git remote set-url origin https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/SpiGetOrg/Documentation.git
 git add swagger.json
 git push
