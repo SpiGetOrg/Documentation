7800c45eef3563698df19968c171e5ac516c0006
Haylee Schäfer <mail@inventivetalent.org>
2017-01-03 20:31:05 +0100

    Delete push.sh

--- .travis/push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git config user.name "Travis"
-git config user.email "${GITHUB_EMAIL}"
-
-git init
-git add swagger.json
-git commit -m "[CI] Generate swagger.json" || echo "Commit failed (There probably were no changes)"
-git push --force "https://InventiveBot:${GITHUB_TOKEN}@${GITHUB_REPO}" master
