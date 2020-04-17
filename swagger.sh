c51ec30cd1726938b20336e16fb8a6b9cb79bdf3
Haylee Schäfer <mail@inventivetalent.org>
2020-04-17 13:18:55 +0200

    Update swagger.sh

--- swagger.sh
+++ swagger.sh
@@ -11,6 +11,9 @@ git checkout master
 wget -nc -O swagger-codegen-cli.jar https://oss.sonatype.org/content/repositories/releases/io/swagger/swagger-codegen-cli/2.2.1/swagger-codegen-cli-2.2.1.jar || echo "swagger-codegen already downloaded"
 java -jar swagger-codegen-cli.jar generate -i swagger.yml -l swagger
 
+# Remove files we don't want
+rm swagger-codegen-cli.jar
+
 # Commit file & push
 git add swagger.json
 git commit -m "Generate swagger.json [ci skip]" || echo "Commit failed (There probably were no changes)"
