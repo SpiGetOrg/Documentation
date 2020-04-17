d9812d0e764431eb5588aa8716328d4327acdf13
Haylee Schäfer <mail@inventivetalent.org>
2020-04-17 13:13:04 +0200

    Update swagger.sh

--- swagger.sh
+++ swagger.sh
@@ -8,10 +8,9 @@ git config user.email "${GIT_EMAIL}"
 git checkout master
 
 # Generate swagger JSON
-wget -nc http://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.1.6/swagger-codegen-cli-2.1.6.jar -O swagger-codegen-cli.jar || echo "swagger-codegen already downloaded"
+wget -nc -O swagger-codegen-cli.jar https://oss.sonatype.org/content/repositories/releases/io/swagger/swagger-codegen-cli/2.2.1/swagger-codegen-cli-2.2.1.jar || echo "swagger-codegen already downloaded"
 java -jar swagger-codegen-cli.jar generate -i swagger.yml -l swagger
 
 # Commit file & push
 git add swagger.json
 git commit -m "Generate swagger.json [ci skip]" || echo "Commit failed (There probably were no changes)"
-git push --force --quiet "https://InventiveBot:${GITHUB_TOKEN}@${GIT_REPO}" master
